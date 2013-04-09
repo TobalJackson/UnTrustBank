9ecb493037e3ee511da4d36b1846e909b9377a2d
Chris James <tobaljackson@gmail.com>
2013-04-08 23:42:52 -0400

    implemented the interface, which applies to all acounts except LOC and loan, allows the customer to directly withdraw an amount from the account.

--- UnTrustBank/src/accountTypes/CustomerTransferSource.java
+++ UnTrustBank/src/accountTypes/CustomerTransferSource.java
@@ -1,5 +1,6 @@
 package accountTypes;
-
+import bank.Transaction;
 public interface CustomerTransferSource {
 //	public void transferBetweenOwnAccounts(double amount, BasicAccount source, BasicAccount destination);
+	public Transaction customerTransferWithdrawal(double amount);
 }
