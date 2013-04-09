342796f9eab9dd6de1e0d860358df421fecd9336
Chris James <tobaljackson@gmail.com>
2013-04-09 18:43:26 -0400

    Started implementing Savings, extended it from BasicAccount and implemented transfer source method.

--- UnTrustBank/src/accountTypes/SavingsAccount.java
+++ UnTrustBank/src/accountTypes/SavingsAccount.java
@@ -1,5 +1,13 @@
 package accountTypes;
 
-public class SavingsAccount implements CustomerTransferSource{
+import bank.Request;
+import bank.Transaction;
+
+public class SavingsAccount extends BasicAccount implements CustomerTransferSource, WithdrawRequestable{
+	public void requestWithdrawal(double amount){
+		if (this.getCurrentAccountBalance() - amount > 0){
+			Request r = new Request(this, amount, this.getAccountOwner(), Transaction.WITHDRAWAL);
+		}
+	}
 //intitial positive transaction required in constructor
 }
