678fb2dacc287668574698ba83e8a395f310802f
Michael Silberling <Michael Silberling@10.136.69.176>
2013-04-12 16:11:56 -0400

    negative withdrawls, deposits in the CustomerUserDriver.

--- UnTrustBank/src/drivers/CustomerUserDriver.java
+++ UnTrustBank/src/drivers/CustomerUserDriver.java
@@ -42,24 +42,38 @@ public class CustomerUserDriver {
 		System.out.println("dollabills: " + dollabills.getCurrentAccountBalance());
 		System.out.println(dollabills.getCurrentAccountBalance());
 		
-		System.out.println(bob.getFirstName());
-		System.out.println(bob.getMiddleName());
-		System.out.println(bob.getLastName());
-		if(bob.getIsMale()){
-			System.out.println("Male");
-		}
-		else{
-			System.out.println("Female");
-		}
-		System.out.println(dobbob.getYear());
-		System.out.println(dobbob.getMonth());
-		System.out.println(dobbob.getDay());
-		System.out.println(bob.getCustomerSSN(Chris));
-		System.out.println(bob.getUsername());
-		System.out.println(bob.getUserID());
+//		System.out.println(bob.getFirstName());
+//		System.out.println(bob.getMiddleName());
+//		System.out.println(bob.getLastName());
+//		if(bob.getIsMale()){
+//			System.out.println("Male");
+//		}
+//		else{
+//			System.out.println("Female");
+//		}
+//		System.out.println(dobbob.getYear());
+//		System.out.println(dobbob.getMonth());
+//		System.out.println(dobbob.getDay());
+//		System.out.println(bob.getCustomerSSN(Chris));
+//		System.out.println(bob.getUsername());
+//		System.out.println(bob.getUserID());
 		
+		// testing exceptions
+		//Negative deposit
+		Transaction moneyzdosD = new Transaction(-7000000, bob, sally, 1); // still allows a negative deposit
+		sally.deposit(moneyzdosD, dollabills);
+		dollabills.appendTransaction(moneyzdosD, sally);
+		dollabills.updateCurrentAccountBalance();
+		System.out.println("moneyzdosD: " + moneyzdosD.getAmount());
+		System.out.println("dollabills: " + dollabills.getCurrentAccountBalance());
 		
-		
+		//Negative withdrawal
+		Transaction moneyzdosW = new Transaction(-7000000, bob, sally, 0); // still allows a negative deposit
+		sally.deposit(moneyzdosW, dollabills);
+		dollabills.appendTransaction(moneyzdosW, sally); //
+		dollabills.updateCurrentAccountBalance();
+		System.out.println("moneyzdosW: " + moneyzdosW.getAmount());
+		System.out.println("dollabills: " + dollabills.getCurrentAccountBalance());
 	}
 
 }
